a8b2cb74f8d0466e5729b6f7d14a89f6ba83d557
ajz003 <ajz003@ucla.edu>
2018-09-18 09:42:19 -0700

    Edit questions and add start inquirer

--- flashcard.js
+++ flashcard.js
@@ -9,10 +9,10 @@ var decBasic = new basic("In what year was the Declaration of Independence ratif
 var whiteHouseBasic = new basic("Who was the first president to live in the White House?", "John Adams", "basic")
 var fourthFireWorksBasic = new basic("In what city was the first Fourth of July celebration with a fireworks display was held?", "Boston", "basic")
 
-var civilCloze = new cloze("The American Civil War started in 1861.", "1861", "cloze")
+var civilCloze = new cloze("The American Civil War started in the year 1861.", "1861", "cloze")
 var flagCloze = new cloze("Betsy Ross was credited for creating the first American flag.", "Betsy Ross", "cloze")
-var gettyCloze = new cloze('"Four score and seven years ago" appears in the Gettysburg Address.', "Gettysburg Address", "cloze")
-var anthemCloze = new cloze("Francis Scott Key wrote the lyrics to the national anthem of the USA.", "Francis Scott Key", "cloze")
+var gettyCloze = new cloze('"Four score and seven years ago" appears in the Gettysburg Address.', "Gettysburg", "cloze")
+var anthemCloze = new cloze("Francis Scott Key wrote the lyrics to the national anthem of the USA. (First Middle Last)", "Francis Scott Key", "cloze")
 var birdCloze = new cloze("The turkey was the bird that Benjamin Franklin said would be more appropriate than the eagle to be the national symbol.", "turkey", "cloze")
 
 
@@ -21,10 +21,36 @@ var birdCloze = new cloze("The turkey was the bird that Benjamin Franklin said w
 var questionsArr = [thirdPresidentBasic, civilCloze, decBasic, flagCloze, whiteHouseBasic, gettyCloze, fourthFireWorksBasic, anthemCloze, sixteenPresidentBasic, birdCloze]
 
 var onQuestion = 0;
+var correct = 0;
+var wrong = 0;
 
-presentQuestion();
+function init() {
+
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "confirm",
+            message: "Ready to start studying?"
+        }
+    ]).then(function (response) {
+
+        if (response.confirm) {
+            console.log("Great! Let's get started. Remember, the answers are CaSe-SeNsItIvE!")
+            presentQuestion();
+        }
+        else {
+            console.log("See you next time then!")
+        }
+
+    });
+}
+
+init();
+
 
 function presentQuestion() {
+
+
 
     if (onQuestion < 10) {
 
@@ -38,16 +64,18 @@ function presentQuestion() {
             {
                 type: "input",
                 name: "question",
-                message: questionsArr[onQuestion].front
+                message: "Question " + (onQuestion+1) + ": " + questionsArr[onQuestion].front
             }
         ]).then(function (response) {
 
             if (response.question === questionsArr[onQuestion].back) {
                 console.log("Correct!");
+                correct++;
 
             } else {
                 console.log("Wrong!");
                 console.log("The correct answer was " + questionsArr[onQuestion].back  + ".");
+                wrong++;
             }
             onQuestion++;
             presentQuestion();
@@ -61,16 +89,18 @@ function presentQuestion() {
             {
                 type: "input",
                 name: "question",
-                message: questionsArr[onQuestion].partial
+                message: "Question " + (onQuestion+1) + ": " + questionsArr[onQuestion].partial
             }
         ]).then(function (response) {
 
             if (response.question === questionsArr[onQuestion].cloze) {
                 console.log("Correct!");
+                correct++;
 
             } else {
                 console.log("Wrong!");
                 console.log("The correct answer was " + questionsArr[onQuestion].cloze + ".");
+                wrong++;
 
             }
             onQuestion++;
@@ -80,7 +110,10 @@ function presentQuestion() {
     }
 
     } else {
+        console.log("-----------------------")
         console.log("Game over.");
+        console.log("You got " + correct + " questions right and " + wrong  + " questions wrong.")
+        console.log("Your final score is " + correct + "/10.")
         return;
     }
 
